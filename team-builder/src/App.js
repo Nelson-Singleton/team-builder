f80ed1f28e159610a465a3cb555d2e9073c2d011
nelson-singleton <nsingleton@ymail.com>
2020-08-18 23:08:45 -0500

    State updates properly. Currently working on submit button

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,34 +2,67 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-const myTeam = [{"name": "Nelson S", "e-mail":"nelson@nelson.com", "role": "student"}]
+const myTeam = [{"name": "Nelson S", "email":"nelson@nelson.com", "role": "student"}]
 
 const [teamList, setTeamList] = useState(myTeam);
 // const changeTeam = event => {
 //   setTeamList(event.target.value);
 // };
 
-console.log(myTeam)
-console.log(teamList)
 const handleChange = event => {
   setTeamList(event.target.value);
 }
 
-const submitForm = () => {
+const nameToState = event => {
+  setTeamList({ ...teamList, name: event.target.value });
+};
+
+const emailToState = event => {
+  setTeamList({ ...teamList, email: event.target.value });
+};
+
+const roleToState = event => {
+  setTeamList({ ...teamList, role: event.target.value });
+};
+
+const submitForm = event => {
+  event.preventDefault();
   
+
 } 
-const [name, setName] = useState("");
+
+// const [name, setName] = useState("");
+
+console.log(myTeam)
+console.log(teamList)
+
+
+
+
+
 
 
   return (
 
 
     <div className="App">
-      <form>
+      <form onSubmit = {event => submitForm(event)}>
         <label>
           Username:
-          <input type="text" onChange= {event => handleChange(event)} />
+          <input type="text" name="name" onChange= {event => nameToState(event)} />
         </label>
+        <br></br>
+        <label>
+          E-mail:
+          <input type="text" name="email" onChange= {event => emailToState(event)} />
+        </label>
+        <br></br>
+        <label>
+          Role:
+          <input type="text" name="role" onChange= {event => roleToState(event)} />
+        </label>
+        <br></br> <br></br>
+        <button > Submit </button>
       </form>
     </div>
   );
